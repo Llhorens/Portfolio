a44ee49532fc693e4d876697526f4c2cc5506f88
Llhorens <85550607+Llhorens@users.noreply.github.com>
2021-09-07 09:17:42 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="src/img/logo3.ico" />
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="src/animate/animate.css">
+
+    <title>Portfolio-Ludovic-LHORENS</title>
+</head>
+
+<body>
+    <header>
+
+        <!-- Bannière LOGO à gauche avec menu -->
+
+        <div class="bannerMenu wow slideInDown" data-wow-delay="0.1s">
+
+            <div class="logo">
+                <a href="main.php"><img class="logo" src="src/img/logo.png" alt="logo"></a>
+            </div>
+            <div class="line">
+
+            </div>
+            <div id="skillsButton">
+                <a href="#rf_competences">Compétences</a>
+            </div>
+            <div id="projectsButton">
+                <a href="#bf_projet1">Projets</a>
+            </div>
+            <div id="contactButton">
+                <a href="#bf_contact_prenom_nom">Contact</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="grille">
+
+        <!-- <section class="accueil"> -->
+
+        <div class="red_frame wow slideInRight" data-wow-delay="0.4s" id="rf_bienvenue">
+            <div class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p><span class="color_swap">#</span>Bienvenue . . .<span class="typing-cursor"></span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="blue_frame wow slideInLeft" data-wow-delay="0.6s" id="bf_voyageur">
+            <div class="blue_frame_border">
+                <div class="blue_frame_bg">
+                    <div class="blue_frame_text">
+                        <p>voyageur:<span class="color_swap">~$</span> php
+                        <h1>portfolio_llhorens.php</h1>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="red_frame wow slideInRight" data-wow-delay="0.8s" id="rf_iam">
+            <div class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p> <span class="color_swap">#</span>Je suis <span class="wow iam_text" data-wow-delay="0.6s"></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="blue_frame wow slideInLeft" data-wow-delay="1s" id="bf_parcours">
+            <div class="blue_frame_border">
+                <div class="blue_frame_bg">
+                    <div class="blue_frame_text">
+                        <p> <span class="color_swap">~$</span>Mon parcours : <br><br>
+
+                            Dévellopeur Web Full Stack...<br><br>
+
+                            <span class="color_swap">./</span> BTS système numérique.. <br><br>
+
+                            <span class="color_swap">./</span> Formation ACS OnlineFormaPro. <br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        -
+
+
+        <div class="red_frame wow slideInRight" data-wow-delay="1s" id="rf_competences">
+            <div class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p> <span class="color_swap">#</span>Mes compétences : <br>
+                            Développeur Full Stacks . . . </p>
+                        <p><span class="color_swap">./</span> .. HTML5 - - CSS3 - -</p>
+
+                        <p><span class="color_swap">./</span> .. ADOBE Ui - - MySQL - -</p>
+
+                        <p><span class="color_swap">./</span> .. ADOBE Ps - - PHP7 - -</p>
+
+                        <p><span class="color_swap">./</span> .. JavaScript - - # </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- </section> -->
+
+
+        <!-- bannière LOGO à droite -->
+        <div class="bannerDroite wow slideInLeft" data-wow-delay="1s">
+            <div class="line">
+
+            </div>
+            <div class="logo">
+                <a href="main.php"><img class="logo" src="src/img/logo.png" alt="logo"></a>
+            </div>
+        </div>
+
+
+        <!-- <section class="projet"> -->
+        <div class="blue_frame wow slideInLeft" data-wow-delay="0.6s" id="bf_projet1">
+            <div id="#projects" class="blue_frame_border">
+                <div class="blue_frame_bg">
+                    <div class="blue_frame_text">
+                        <p><span class="color_swap">~$</span> service start projet_widget<span class="color_swap"></span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="img_projet wow slideInUp" data-wow-delay="1s" id="img_projet_1">
+            <img src="src/img/projet_1.jpg" alt="image projet widget">
+        </div>
+
+
+        <div class="red_frame wow slideInRight" data-wow-delay="0.6s" id="rf_projet1">
+            <div id="skills" class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p> <span class="color_swap">#</span>projet_widget : </p>
+
+                        <p>Le premier projet que j'ai réalisé en groupe, comprend un site internet incluant trois
+                            widgets, météo, calculatrice et recherche.</p>
+
+                        <p> Réalisé en HTML5, CSS3 et JavaScript
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blue_frame wow slideInLeft" data-wow-delay="0.6s" id="bf_projet2">
+            <div id="#projects" class="blue_frame_border">
+                <div class="blue_frame_bg">
+                    <div class="blue_frame_text">
+                        <p><span class="color_swap">~$</span>service start projet_<span class="color_swap">######</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="img_projet wow slideInUp" data-wow-delay="1s" id="img_projet_2">
+            <img src="src/img/coming_soon.jpg" alt="coming soon">
+        </div>
+
+
+        <div class="red_frame wow slideInRight" data-wow-delay="0.6s" id="rf_projet2">
+            <div id="skills" class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p> <span class="color_swap">#</span>projet_<span class="color_swap">######</span> : <br /><br />
+                            pcjQDcRQ5E
+                            9nUlgTvx5T
+                            7qwdLptEWc
+                            fOviSkTW1Y
+                            sIpN5OWLv9
+                            NwQbiguvF9
+                            T6k9VDYGvr
+                            zEns9No10a
+                            EhpOZB26gP
+                            trfrHaxUdu
+                            clF2AqrnGJ
+                            ktcWPg6X2z
+                            z9myBQvvzn
+                            EGB0i9ugUh
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="blue_frame wow slideInLeft" data-wow-delay="0.6s" id="bf_projet3">
+            <div id="#projects" class="blue_frame_border">
+                <div class="blue_frame_bg">
+                    <div class="blue_frame_text">
+                        <p><span class="color_swap">~$</span> service start projet_<span class="color_swap">######</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="img_projet wow slideInUp" data-wow-delay="1s" id="img_projet_3">
+            <img src="src/img/coming_soon.jpg" alt="coming soon">
+        </div>
+
+
+        <div class="red_frame wow slideInRight" data-wow-delay="0.6s" id="rf_projet3">
+            <div id="skills" class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p> <span class="color_swap">#</span> projet_<span class="color_swap">######</span> : <br /><br />
+                            pcjQDcRQ5E
+                            9nUlgTvx5T
+                            7qwdLptEWc
+                            fOviSkTW1Y
+                            sIpN5OWLv9
+                            NwQbiguvF9
+                            T6k9VDYGvr
+                            zEns9No10a
+                            EhpOZB26gP
+                            trfrHaxUdu
+                            clF2AqrnGJ
+                            ktcWPg6X2z
+                            z9myBQvvzn
+                            EGB0i9ugUh
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- </section> -->
+
+
+        <!-- bannière LOGO à Gauche -->
+        <div class="bannerGauche wow slideInRight" data-wow-delay="0.6s">
+            <div class="logo">
+                <a href="main.php"><img class="logo" src="src/img/logo.png"></a>
+            </div>
+            <div class="line">
+
+            </div>
+
+        </div>
+
+        <!-- <section class="contact"> -->
+
+        <div class="red_frame wow slideInRight" data-wow-delay="1s" id="rf_contact">
+            <div class="red_frame_border">
+                <div class="red_frame_bg">
+                    <div class="red_frame_text">
+                        <p><span class="color_swap">#</span>Contact . . . </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <form class="form_contact wow slideInUp" data-wow-delay="0.6s" method="post">
+            <div class="blue_frame" id="bf_contact_prenom_nom">
+                <div class="blue_frame_border">
+                    <div class="blue_frame_bg">
+                        <div class="blue_frame_text">
+                            <label for="name">Nom :</label>
+                            <input type="text" id="name" name="name" size="50" placeholder=" Michel Dubois" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="blue_frame " id="bf_contact_sujet">
+                <div class="blue_frame_border">
+                    <div class="blue_frame_bg">
+                        <div class="blue_frame_text">
+                            <label for="subject">Sujet :</label>
+                            <input type="text" id="subject" name="subject" size="50" placeholder=" Devis,emplois..." required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="blue_frame" id="bf_contact_email">
+                <div class="blue_frame_border">
+                    <div class="blue_frame_bg">
+                        <div class="blue_frame_text">
+                            <label for="email">Email :</label>
+                            <input type="email" id="email" name="email" size="30" placeholder=" <a> exemple@gmail.com" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="blue_frame" id="bf_contact_msg">
+                <div class="blue_frame_border">
+                    <div class="blue_frame_bg">
+                        <div class="blue_frame_text">
+                            <label for="msg">Message :</label>
+                            <textarea type="text" id="msg" name="msg" size="2000" placeholder=" <p> Votre message ici . . . " required></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class="yellow_frame" id="yf_button_send" type="submit">
+                <div class="yellow_frame_border">
+                    <div class="yellow_frame_bg">
+                        <div class="yellow_frame_text">
+                            <p> Envoyer </p>
+                        </div>
+                    </div>
+                </div>
+            </button>
+        </form>
+
+        <!-- </section> -->
+    </div>
+    <?php
+    if (isset($_POST['msg'])) {
+        $position_arobase = strpos($_POST['email'], '@');
+        if ($position_arobase === false)
+            echo '<p>Votre email doit comporter un arobase.</p>';
+        else {
+            $retour = mail('ludoviclhorens@gmail.com', 'Envoi depuis la page Contact du portfolio. Sujet : ', $_POST['subject'], 'Par: ' . $_POST['name'] . $_POST['email'], 'Message :' . $_POST['msg']);
+            if ($retour)
+                echo '<p>Votre message a été envoyé.</p>';
+            else
+                echo '<p> Hello There </p>';
+        }
+    }
+    ?>
+    <footer>
+        <div class="line">
+
+        </div>
+        <div class="reseau">
+            <a href="https://www.linkedin.com/in/ludovic-lhorens-b05178177/"> LinkedIn : Ludovic Lhorens </a>
+            <a href="https://github.com/Llhorens"> GitHub : Llhorens </a>
+            ©LudovicLhorens
+        </div>
+    </footer>
+
+
+
+    <script src="src/wow/wow.min.js"></script>
+    <script src="src/typed.js-master/lib/typed.min.js"></script>
+    <script>
+        new WOW().init();
+        var type = new Typed('.iam_text', {
+            strings: ['Front End Dev', 'Back End Dev', 'Ludovic Lhorens'],
+            typeSpeed: 120,
+            loop: true
+        });
+        var type = new Typed('.typing-cursor', {
+            strings: [''],
+            typeSpeed: 80,
+
+        });
+    </script>
+</body>
+
+</html>
